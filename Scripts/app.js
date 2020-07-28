cd87380393a36c7062161c405c30bf20af1a7ad2
Andres Mejia <afmm26@hotmail.es>
2020-07-28 16:43:42 -0500

    Cambios

---
+++ Scripts/app.js
@@ -0,0 +1,8 @@
+class App extends React.Component {
+  render() {
+    return <h1>hey ya de buena </h1>;
+  }
+}
+
+const rootElement = document.getElementById("app");
+ReactDOM.render(<App />, rootElement);
